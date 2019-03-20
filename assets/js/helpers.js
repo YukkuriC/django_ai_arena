63f633312b4a39d4fe4f401a17ca30c2ace386a5
Yukkuri C <799433638@qq.com>
2019-03-20 20:38:28 +0800

    手气不错

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,4 +1,9 @@
 // 刷新消息推送区域
-function update_messages(){
+function update_messages() {
     $('#message_block').load('/msg/')
+}
+
+// randrange
+function randrange(x) {
+    return Math.floor(Math.random() * x)
 }
