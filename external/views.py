1cbf1a706bd35317d95b5b9b6ca7bd6b9aff07c8
Yukkuri C <799433638@qq.com>
2020-05-30 02:23:23 +0800

    通用化记录浏览页
    - 读取文件修改时间
      - 根据GET参数决定按文件名或修改时间排序展示
    - 读取记录对象本身
      - 可增加更多可视化接口

--- external/views.py
+++ external/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from main import settings
 from main.helpers import sorry
+from .helpers_core import stringfy_error
 from .factory import Factory
-import os, pickle, zlib, json
+import os, time
 
 
 def course_results(request, AI_type, folder):
@@ -14,6 +15,7 @@ def course_results(request, AI_type, folder):
         return sorry(request, text='无效的游戏类型')
     title = settings.AI_TYPES[AI_type]
     request.session['curr_game'] = AI_type  # 设置当前页面游戏
+    loader = Factory(AI_type)
 
     file_folder = '%s/results/%03d' % (settings.MEDIA_ROOT, AI_type)
     if not os.path.isdir(file_folder):
@@ -30,15 +32,31 @@ def course_results(request, AI_type, folder):
     folders, files = [], []
     for f in pool:
         f1 = os.path.join(file_folder, f)
-        if os.path.isfile(f1):
-            files.append(f)
+        if os.path.isfile(f1):  # 读取记录
+            try:
+                rec_unit = {
+                    'name': f,  # 记录名称
+                    'record': loader.load_record_path(f1),  # 记录对象
+                    'time': time.ctime(os.path.getmtime(f1)),  # 修改时间
+                }
+                files.append(rec_unit)
+            except Exception as e:
+                messages.warning(request, f'读取{f}时出错: {stringfy_error(e)}')
         else:
             folders.append(f)
-    debug = {
-        'type': AI_type,
-        'dir': file_folder,
-        'pool': pool,
-    }
+
+    # 根据GET参数决定排序方式
+    sort_method = request.GET.get(
+        'sort',
+        request.session.get('course_results_sort', 'name'),
+    )
+    request.session['course_results_sort'] = sort_method  # 缓存参数于session中
+    sort_keys = {'name': '名称', 'time': '时间'}
+    for key in sort_keys:
+        if sort_method == key:
+            files.sort(key=lambda x: x[key])
+            break
+
     return render(request, 'results/filetree.html', locals())
 
 
