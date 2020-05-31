2c27e4fe6735ebb1bacbc9b1072405857e8c411d
Yukkuri C <799433638@qq.com>
2020-06-01 01:56:36 +0800

    记录查看页增加详细统计接口
    - 已对2048适配

--- external/views.py
+++ external/views.py
@@ -1,10 +1,13 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape as esc
 from main import settings
 from main.helpers import sorry
 from .helpers_core import stringfy_error
 from .factory import Factory
 import os, time
+from collections import Counter
 
 
 def course_results(request, AI_type, folder):
@@ -49,6 +52,9 @@ def course_results(request, AI_type, folder):
     for record in files:
         record['tags'] = loader.analyze_tags(record['record'])
 
+    # 加载额外信息
+    extra = extra_info(AI_type, files)
+
     # 根据GET参数决定排序方式
     sort_method = request.GET.get(
         'sort',
@@ -76,3 +82,46 @@ def course_view(request, AI_type, file):
     record = loader.load_record_path(file)
     record_content = loader.stringfy_record_obj(record)
     return render(request, 'renderer/%s.html' % AI_type, locals())
+
+
+def extra_info(AI_type, records):
+    """
+    解析额外信息
+    返回待显示信息
+    """
+    logs = []
+
+    if AI_type == 4:
+        extra_info_2048(records, logs)
+
+    return logs
+
+
+def extra_info_2048(records, logs):
+    """
+    统计2048双方比分等信息
+    写入每局发起方至extra
+    """
+    score_counter = Counter()  # 计分
+
+    for record_pack in records:
+        record = record_pack['record']
+        names = [
+            os.path.basename(os.path.splitext(record[f'name{i}'])[0])
+            for i in range(2)
+        ]
+        record_extra = f'发起方: {names[0]}; '
+        if record['winner'] == None:
+            record_extra += '平局'
+            for n in names:
+                score_counter[n] += 0.5
+        else:
+            winner = names[record['winner']]
+            score_counter[winner] += 1
+            record_extra += f'胜者: {winner}'
+        record_pack['extra'] = record_extra  # 写入额外信息
+
+    # 组装比分字符串
+    base = ' : '.join(f'''<span title='{esc(k)}'>{v}</span>'''
+                      for k, v in score_counter.items())
+    logs.append(mark_safe(f'<h2 style="text-align:center">{base}</h2>'))
